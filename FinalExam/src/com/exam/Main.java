dd7b7bfebc7d1a1dcf6b105ce878f7cf7b0525da
annachen88 <32476353+annachen88@users.noreply.github.com>
2018-06-26 15:36:24 +0800

    b

--- FinalExam/src/com/exam/Main.java
+++ FinalExam/src/com/exam/Main.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 
 public class Main {
 static List<Food> list = new ArrayList<>();
+static List<String> list2 = new ArrayList<>();
 	public static void main(String[] args) {
 try {
 	FileReader read = new FileReader("menu.txt");
@@ -29,12 +30,15 @@ try {
 	// TODO Auto-generated catch block
 	e.printStackTrace();
 }
-print();
+while(true){print();}
 
 	}
 public static void print(){
-	for(int i=0; i<list.size();i++){
+	
+	int i=0;
+	while(i<list.size()){
 		System.out.println(list.get(i).getNumber()+")"+list.get(i).getFood());
+		i++;
 	}
 	System.out.println("0) 結算");
 	System.out.println("q) 離開(結束程式)");
@@ -43,6 +47,26 @@ public static void print(){
 	String a = scanner.nextLine();
 	System.out.println("請輸入數量:");
 	int b = scanner.nextInt();
+	i=0;
+	boolean check=true;
+	while(check){
 	
-}
-}
+	//new Integer(list.get(i).getNumber()).toString()
+	if(Integer.parseInt(a)==list.get(i).getNumber()){
+		//System.out.println("目前餐點:"+list.get(i).getFood()+b+"份");	
+		//list.get(i).getPrice()*b
+		list2.add(list.get(i).getFood()+b);
+		System.out.println("目前餐點:");
+		for(int j =0; j<list2.size();j++)
+			System.out.println((j+1)+"."+" " +list2.get(j)+"份");
+		check=false;
+	}else{
+		i=i+1;}
+	}
+	//list2.add(list.get(i).getFood()+b);
+	System.out.println("=================");
+	//for(int j =0; j<list2.size();j++)
+	//System.out.println((j+1)+"."+" " +list2.get(j)+"份");
+	
+}}
+
